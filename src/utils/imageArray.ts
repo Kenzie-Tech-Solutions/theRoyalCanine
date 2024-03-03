1af39dfa9a63c57fab5c59b16c018ebcb7f81eb7
Iconians <claytoncripe@gmail.com>
2024-03-02 17:29:11 -0800

    made hyperlink for youtube video and added the new images to imageArray and made sure they all work as they should

--- src/utils/imageArray.ts
+++ src/utils/imageArray.ts
@@ -6,7 +6,24 @@ import img3 from "../assets/image3.jpeg";
 import img4 from "../assets/image4.jpeg";
 import img5 from "../assets/image5.jpeg";
 import img6 from "../assets/image6.jpeg";
-// imported images
+import anotherHappyFarmDog from "../assets/anotherHappyFarmDog.jpg";
+import babushkaJpg from "../assets/babushka.jpg";
+import castleFiltered from "../assets/castleFiltered.jpg";
+import castleInstructions from "../assets/castleInstructions.jpg";
+import cleaningUp from "../assets/cleaningUp.jpg";
+import dogHair from "../assets/dogHair.jpg";
+import easyPeasy from "../assets/easyPeasy.jpg";
+import faveCustomers from "../assets/faveCustomers.jpg";
+import greta from "../assets/greta.png";
+import kennyMfRogers from "../assets/kennyMfRogers.jpg";
+import margo from "../assets/margo.jpg";
+import newPuppy from "../assets/newPuppy.jpg";
+import oillie from "../assets/ollie.png";
+import pacemanHelping from "../assets/pacmanHelping.jpg";
+import pig from "../assets/pig.jpg";
+import posing from "../assets/posing.jpg";
+import seating from "../assets/seating.jpg";
+import sweatestingSupportPup from "../assets/sweetestSupportPup.jpg";
 
 export const imageArray = [
   {
@@ -16,7 +33,7 @@ export const imageArray = [
     alt: "dog",
     title: "Dog 1",
     description:
-      "This is a dog. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec nisl aliquam nunc, nec Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies ultricies, nunc nisl aliquam nunc, nec",
+      "This is a dog.",
   },
   {
     id: 1,
@@ -65,6 +82,132 @@ export const imageArray = [
     src: img6,
     alt: "dog",
     title: "Dog 6",
+    description: "This is a dog",
+  },
+  {
+    id: 8,
+    src: anotherHappyFarmDog,
+    alt: "another Happy Farm Dog",
+    title: "Another Happy Farm Dog",
+    description: "This is a dog",
+  },
+  {
+    id: 9,
+    src: babushkaJpg,
+    alt: "babushka",
+    title: "Babushka",
+    description: "This is a dog",
+  },
+  {
+    id: 10,
+    src: castleFiltered,
+    alt: "babushka",
+    title: "Babushka",
+    description: "This is a dog",
+  },
+  {
+    id: 11,
+    src: dogHair,
+    alt: "dog Hair",
+    title: "Dog Hair",
+    description: "This is a dog",
+  },
+  {
+    id: 12,
+    src: castleInstructions,
+    alt: "castle Instructions",
+    title: "Castle Instructions",
+    description: "This is a dog",
+  },
+  {
+    id: 13,
+    src: cleaningUp,
+    alt: "cleaning Up",
+    title: "Cleaning Up",
+    description: "This is a dog",
+  },
+  {
+    id: 14,
+    src: easyPeasy,
+    alt: "easy Peasy",
+    title: "Easy Peasy",
+    description: "This is a dog",
+  },
+  {
+    id: 15,
+    src: faveCustomers,
+    alt: "fave Customers",
+    title: "Fave Customers",
+    description: "This is a dog",
+  },
+  {
+    id: 16,
+    src: greta,
+    alt: "greta",
+    title: "Greta",
+    description: "This is a dog",
+  },
+  {
+    id: 17,
+    src: kennyMfRogers,
+    alt: "kenny mf Rogers",
+    title: "Kenny mf Rogers",
+    description: "This is a dog",
+  },
+  {
+    id: 18,
+    src: margo,
+    alt: "margo",
+    title: "Margo",
+    description: "This is a dog",
+  },
+  {
+    id: 19,
+    src: newPuppy,
+    alt: "new Puppy",
+    title: "New Puppy",
+    description: "This is a dog",
+  },
+  {
+    id: 20,
+    src: oillie,
+    alt: "oillie",
+    title: "Oillie",
+    description: "This is a dog",
+  },
+  {
+    id: 21,
+    src: pacemanHelping,
+    alt: "paceman Helping",
+    title: "Paceman Helping",
+    description: "This is a dog",
+  },
+  {
+    id: 22,
+    src: pig,
+    alt: "pig",
+    title: "Pig",
+    description: "This is a dog",
+  },
+  {
+    id: 23,
+    src: posing,
+    alt: "posing",
+    title: "Posing",
+    description: "This is a dog",
+  },
+  {
+    id: 24,
+    src: seating,
+    alt: "seating",
+    title: "Seating",
+    description: "This is a dog",
+  },
+  {
+    id: 25,
+    src: sweatestingSupportPup,
+    alt: "sweatesting Support Pup",
+    title: "Sweatesting Support Pup",
     description: "This is a dog",
   },
 ];
