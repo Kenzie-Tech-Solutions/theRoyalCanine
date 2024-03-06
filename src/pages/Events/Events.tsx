47743c82079568eb76edcb0343cc126395b6cbca
Iconians <claytoncripe@gmail.com>
2024-03-06 12:06:41 -0800

    changed the event page cards to a similar design as the homepage cards

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -2,6 +2,12 @@ import { Footer } from "../../Components/Footer/Footer";
 import { MenuModal } from "../../Components/MenuModal/MenuModal";
 import { NavBar } from "../../Components/NavBar/NavBar";
 import { eventDetails } from "../../utils/eventDetails";
+import {
+  faClock,
+  faLocationDot,
+  faCalendarAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Events.module.css";
 
 export const Events = () => {
@@ -15,10 +21,32 @@ export const Events = () => {
         <div className={styles.eventsSection}>
           {eventDetails.map((event) => {
             return (
-              <div key={event.date} className={styles.eventCard}>
-                <p>Date: {event.date}</p>
+              <div
+                key={event.date}
+                className={styles.eventCard}
+                style={{ backgroundColor: event.color }}
+              >
                 <h3>{event.title}</h3>
                 <p>{event.details}</p>
+                <div className={styles.flexRow}>
+                  <div>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                  </div>
+                  <p>Location</p>
+                </div>
+                <div className={styles.flexRow}>
+                  <div>
+                    <FontAwesomeIcon icon={faCalendarAlt} />
+                  </div>
+                  <p>{event.date}</p>
+                </div>
+                <div className={styles.flexRow}>
+                  <div>
+                    <FontAwesomeIcon icon={faClock} />
+                  </div>
+                  <p>Time of event</p>
+                </div>
+                <a href="/">event link </a>
               </div>
             );
           })}
