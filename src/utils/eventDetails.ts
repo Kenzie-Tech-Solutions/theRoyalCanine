dbf2a776bd0c3c5e0c984f669db99abb76f34d8b
Michael Sanchez <88916004+mikesz88@users.noreply.github.com>
2024-04-01 08:06:05 -0500

    Update eventDetails.ts

--- src/utils/eventDetails.ts
+++ src/utils/eventDetails.ts
@@ -1,27 +1,5 @@
 export const eventDetails = [
   {
-    date: "Sunday, 3/10/24",
-    title: "Easter Pet “Mini” Photo Shoot",
-    details:
-      "Booked Session Required – Visit the event for the link to book!",
-    color: "grey",
-    location: "The Royal Canine",
-    
-    time: "1 pm – 3 pm",
-    link: "https://www.facebook.com/events/713178987597807"
-  },
-  {
-    date: "Friday, 3/29/24",
-    title: "“Dog Days” Hockey Game – Milwaukee Admirals vs. Rockford IceHogs",
-    details:
-      "Visit the event link for a coupon code!",
-    color: "teal",
-    location: "UW Panther Arena",
-    
-    time: "7 pm",
-    link: "https://www.facebook.com/events/1421731665382732"
-  },
-  {
     date: "Sunday, 4/7/24",
     title: "“Mini-Groom” by Shampooches",
     details:
@@ -32,4 +10,15 @@ export const eventDetails = [
     time: "12 pm – 3 pm",
     link: "https://www.facebook.com/events/3369234966655310"
   },
+  {
+    date: "Friday, 5/17/24",
+    title: "Kickoff to Summer Block Partys",
+    details:
+      "The N. Pine businesses are shutting down the road for a block party! There will be music, food, activities, and fun for all ages (and breeds)!",
+    color: "teal",
+    location: " 500 block of N. Pine St.",
+    
+    time: "5-10 pm",
+    link: "TBD"
+  }
 ];
