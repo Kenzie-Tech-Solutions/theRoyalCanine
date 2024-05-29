c3771dc82f556c6263a7bbd4480a788e9a6dc2f6
Michael Sanchez <88916004+mikesz88@users.noreply.github.com>
2024-05-29 08:21:25 -0500

    Update eventDetails.ts

--- src/utils/eventDetails.ts
+++ src/utils/eventDetails.ts
@@ -1,24 +1,24 @@
 export const eventDetails = [
   {
-    date: "Sunday, 4/7/24",
-    title: "“Mini-Groom” by Shampooches",
+    date: "Saturday, 6/1/24",
+    title: "Loop Day",
     details:
-      "",
+      "Come celebrate the start of summer on the quirky little one-way street that leads to Historic Downtown Burlington! The Loop (the 100 block of E Chestnut St) will be closed to traffic for what's guaranteed to be a fun-filled day of live music, family games and activities, business tours, and all-day food, beverages and craft beer. The Royal Canine will be hosting an “adopt a plushie” activity. Kids can choose from a variety of stuffed “pets” and collars and will be able to decorate their carriers. They will also receive an “adoption certificate” and pet care checklist / chore chart.",
     color: "tan",
-    location: "The Royal Canine",
-    
-    time: "12 pm – 3 pm",
-    link: "https://www.facebook.com/events/3369234966655310"
-  },
-  {
-    date: "Friday, 5/17/24",
-    title: "Kickoff to Summer Block Partys",
-    details:
-      "The N. Pine businesses are shutting down the road for a block party! There will be music, food, activities, and fun for all ages (and breeds)!",
-    color: "teal",
-    location: " 500 block of N. Pine St.",
-    
-    time: "5-10 pm",
-    link: "TBD"
+    location: "The Loop (the 100 block of E Chestnut St)",
+    time: "10am - 10pm",
+    link: "https://www.facebook.com/events/813345654183265"
   }
+  // },
+  // {
+  //   date: "Friday, 5/17/24",
+  //   title: "Kickoff to Summer Block Partys",
+  //   details:
+  //     "The N. Pine businesses are shutting down the road for a block party! There will be music, food, activities, and fun for all ages (and breeds)!",
+  //   color: "teal",
+  //   location: " 500 block of N. Pine St.",
+    
+  //   time: "5-10 pm",
+  //   link: "TBD"
+  // }
 ];
